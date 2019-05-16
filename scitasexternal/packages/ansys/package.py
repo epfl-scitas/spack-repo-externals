1b651e109afb7776860a4e65f716358ec2f990bd
Nicolas Richart <nrichart@users.noreply.github.com>
2019-05-16 15:39:51 +0200

    Adding new version of ansys (#5)

--- scitasexternal/packages/ansys/package.py
+++ scitasexternal/packages/ansys/package.py
@@ -12,23 +12,29 @@ class Ansys(Package):
     licensed = True
     only_binary = True
 
+    version('19.2')
     version('17.1')
 
     def install(self, spec, prefix):
         pass
 
     def setup_environment(self, spack_env, run_env):
-        run_env.prepend_path('PATH', '/ssoft/spack/external/ansys/17.1/v171/ansys/bin')  # noqa: E501
-        run_env.prepend_path('PATH', '/ssoft/spack/external/ansys/17.1/v171/CFD-Pos/bin')  # noqa: E501
-        run_env.prepend_path('PATH', '/ssoft/spack/external/ansys/17.1/v171/CFX/bin')  # noqa: E501
-        run_env.prepend_path('PATH', '/ssoft/spack/external/ansys/17.1/v171/Icepak/bin')  # noqa: E501
-        run_env.prepend_path('PATH', '/ssoft/spack/external/ansys/17.1/v171/TurboGrid/bin')  # noqa: E501
-        run_env.prepend_path('PATH', '/ssoft/spack/external/ansys/17.1/v171/autodyn/bin')  # noqa: E501
-        run_env.prepend_path('PATH', '/ssoft/spack/external/ansys/17.1/v171/fluent/bin')  # noqa: E501
-        run_env.prepend_path('PATH', '/ssoft/spack/external/ansys/17.1/v171/polyflow/bin')  # noqa: E501
-        run_env.prepend_path('PATH', '/ssoft/spack/external/ansys/17.1/v171/tgrid/bin')  # noqa: E501
-        run_env.prepend_path('PATH', '/ssoft/spack/external/ansys/17.1/v171/Framework/bin/Linux64')  # noqa: E501
-        run_env.prepend_path('PATH', '/ssoft/spack/external/ansys/17.1/v171/icemcfd/linux64_amd/bin')  # noqa: E501
+        version = self.spec.version
+        ansys_prefix = '/ssoft/spack/external/ansys/{0}/v{1}'.format(
+            version.up_to(2).dotted(),
+            version.up_to(2).joined())
+        run_env.prepend_path('PATH', join_path(ansys_prefix, 'ansys/bin')
+        run_env.prepend_path('PATH', join_path(ansys_prefix, 'CFD-Pos/bin')
+        run_env.prepend_path('PATH', join_path(ansys_prefix, 'CFX/bin')
+        run_env.prepend_path('PATH', join_path(ansys_prefix, 'Icepak/bin')
+        run_env.prepend_path('PATH', join_path(ansys_prefix, 'TurboGrid/bin')
+        run_env.prepend_path('PATH', join_path(ansys_prefix, 'autodyn/bin')
+        run_env.prepend_path('PATH', join_path(ansys_prefix, 'fluent/bin')
+        run_env.prepend_path('PATH', join_path(ansys_prefix, 'polyflow/bin')
+        run_env.prepend_path('PATH', join_path(ansys_prefix, 'tgrid/bin')  
+        run_env.prepend_path('PATH', join_path(ansys_prefix, 'Framework/bin/Linux64')  # noqa: E501
+        run_env.prepend_path('PATH', join_path(ansys_prefix, 'icemcfd/linux64_amd/bin')  # noqa: E501
 
-        run_env.prepend_path('LD_LIBRARY_PATH', '/ssoft/spack/external/ansys/17.1/v171/Framework/bin/Linux64')  # noqa: E501
-        run_env.prepend_path('LD_LIBRARY_PATH', '/ssoft/spack/external/ansys/17.1/v171/polyflow/polyflow17.1.0/lnamd64/libs')  # noqa: E501
+        run_env.prepend_path('LD_LIBRARY_PATH', join_path(ansys_prefix, 'Framework/bin/Linux64')  # noqa: E501
+        run_env.prepend_path('LD_LIBRARY_PATH', join_path(ansys_prefix, 'polyflow/polyflow17.1.0/lnamd64/libs')  # noqa: E501
+        run_env.prepend_path('LD_LIBRARY_PATH', join_path(ansys_prefix, 'Framework/bin/Linux64/Mesa')  # noqa: E501
