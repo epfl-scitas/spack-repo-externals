4a25b6f5d2c8becb95d4abc98d15e8a6fe2c9586
Nicolas Richart <nicolas.richart@epfl.ch>
2022-04-29 08:17:02 +0200

    making castep compile

--- packages/castep/package.py
+++ packages/castep/package.py
@@ -23,24 +23,38 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
 from spack import *
+import os
 
 
-class Castep(Package):
+class Castep(MakefilePackage):
     """
     CASTEP is a leading code for calculating the properties of materials from
     first principles.
     """
 
-    homepage = "http://www.castep.org/"
-    url = 'fakeurl.tar.gz'
+    homepage = "http://www.castep.org"
+    url = "file://%s/CASTEP-21.11.tar.gz" % os.getcwd()
     licensed = True
-    only_binary = True
 
-    version('21.11')
+    version('21.11', sha256='d909936a51dd3dff7a0847c2597175b05c8d0018d5afe416737499408914728f')
 
     depends_on('intel-mpi')
     depends_on('intel-mkl')
     depends_on('fftw-api@3')
 
+    def build(self, spec, prefix):
+        with working_dir(self.build_directory):
+            make('ROOTDIR={}'.format(self.build_directory),
+                 'FFT=mkl',
+                 'FFTLIBDIR={}'.format(os.environ['MKLROOT']),
+                 'MATHLIBS=mkl',
+                 'MATHLIBDIR={}'.format(os.environ['MKLROOT']),
+                 'ARCH=linux_x86_64_ifort',
+                 'COMMS_ARCH=mpi'
+                 )
+
     def install(self, spec, prefix):
-        pass
+        with working_dir(self.build_directory):
+            make('ROOTDIR={}'.format(self.build_directory),
+                 'INSTALL_DIR={}'.format(prefix),
+                 'install')
